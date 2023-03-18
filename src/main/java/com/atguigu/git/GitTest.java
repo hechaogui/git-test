b15839f18955ccb4bd171d20b70bfca9ce8a24df
fanYuan <fanYuan@atguigu.com>
2023-03-18 16:25:34 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @version 1.0
+ * @auther 何超桂
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
